c89ff125b58085c3a3e8f521db1f9116fa5d41aa
Shrey Talreja <shreytalreja25@gmail.com>
2023-02-24 14:43:23 +0530

    Adding a local controller to view files stored locally in /Reports

---
+++ JasperReportingApp23Feb/Controllers/LocalReportController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JasperReportingApp23Feb.Controllers
+{
+    public class LocalReportController : Controller
+    {
+        // GET: LocalReport
+        public ActionResult ViewReport(string reportName)
+        {
+            string reportPath = Server.MapPath("~/Reports/" + reportName + ".jrxml");
+
+            if (!System.IO.File.Exists(reportPath))
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.ReportPath = reportPath;
+
+            return View();
+        }
+    }
+}
